b55cbd4a59694b44f52e0b295ff95d8a0c554302
Walter Harms <dave@Walters-MacBook-Pro.local>
2016-02-14 07:01:40 -0600

    layed html and css responsive and wireframed

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(function() {
+  'use strict'
+
+  var appToken = 'OB4lP02bNvnjKLmUHoWMdMK6o';
+
+  // $.getJSON('https://data.nashville.gov/resource/dqkw-tj5j.json', function(data) {
+  //   $.each(data, function(index,value) {
+  //     console.log(value.first_name);
+  //   });
+  // })
+
+
+});
