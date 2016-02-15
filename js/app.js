e4359b05d435dbaf5a9563027aa17f527e85702c
Dave Harms <daveharmswebdev@gmail.com>
2016-02-14 21:35:20 -0600

    wrote event clicks. Realized I only need one click

--- js/app.js
+++ js/app.js
@@ -2,6 +2,21 @@ $(function() {
   'use strict'
 
   var appToken = 'TbgePk5hPSZ4RM4nTZjIzsyKwcnq8ep2ajz5Dh7j';
+  console.log(appToken);
 
+  $('.btn-verse').click(function() {
+    var verse = $('.query-verse').val();
+    console.log(verse);
+  });
+
+  $('.btn-firstLanguage').click(function() {
+    var firstLanguage = $('.query-firstLanguage').val();
+    console.log(firstLanguage);
+  });
+
+  $('.btn-secondLanguage').click(function() {
+    var secondLanguage = $('.query-secondLanguage').val();
+    console.log(secondLanguage);
+  });
 
 });
