eed4fb03d79a4f42870b2be3b67b942e9b2ea833
Dave Harms <daveharmswebdev@gmail.com>
2016-02-16 20:52:30 -0600

    retrieves book chapters and verse

--- js/app.js
+++ js/app.js
@@ -1,20 +1,40 @@
 $(function() {
   'use strict'
+  var url = 'http://getbible.net/json';
 
   console.log('hello dave 2');
 
   var settings = function() {
     var settings = {
-      url: 'http://getbible.net/json',
       dataType:"jsonp",
       data: 'p=Jn3:16&v=kjv',
+      data: 'v=kjv',
       type: 'GET',
     }
     return settings;
   };
 
-  var retrieveVerse = function() {
-    $.ajax(settings().url, settings())
+  var retrieveBookNames = function(version) {
+    $.ajax(url, {dataType:'jsonp',data: 'v=' + version,type:'GET'})
+    .done(function(result){
+      $.each(result.version, function(i,item) {
+        console.log(item.book_name);
+      })
+    })
+  }
+
+  var retrieveChapters = function(book) {
+    $.ajax(url, {dataType:'jsonp',data: 'p=' + book,type:'GET'})
+    .done(function(result){
+      console.log(result);
+      $.each(result.book, function(i,item) {
+        console.log(item.chapter_nr);
+      })
+    });
+  }
+
+  var retrieveVerse = function(version,book,chapter,verse) {
+    $.ajax(url, {dataType:'jsonp',data:'p=' + book + chapter + ':' + verse + '&v=' + version,type:'GET'})
     .done(function(result){
       console.log(result);
       // displayVerse(result);
@@ -53,7 +73,9 @@ $(function() {
     console.log(secondLanguage);
   });
 
-  retrieveVerse();
+  retrieveVerse('kjv','John','3','16-18');
+  // retrieveBookNames('kjv');
+  // retrieveChapters('Exodus');
 
 });
 
