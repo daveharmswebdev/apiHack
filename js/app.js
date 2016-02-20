62eb06a41cf9d854af9a4e6580cddb8afc67147c
Dave Harms <daveharmswebdev@gmail.com>
2016-02-20 09:59:42 -0600

    fixed height for items again

--- js/app.js
+++ js/app.js
@@ -1,52 +1,66 @@
 $(function() {
   'use strict'
   var url = 'http://getbible.net/json',
-      version = 'kjv',
-      book = 'Genesis',
-      chapter = '1',
-      verseNum;
+    version = 'kjv',
+    book = 'Genesis',
+    chapter = '1',
+    verseNum;
 
   var retrieveBookNames = function(version) {
-    $.ajax(url, {dataType:'jsonp',data: 'v=' + version,type:'GET'})
-    .done(function(result){
-      $.each(result.version, function(i,item) {
-        $('.bookAndChapter--selectBook').append('<option>'+item.book_name+'</option>');
+    $.ajax(url, {
+        dataType: 'jsonp',
+        data: 'v=' + version,
+        type: 'GET'
       })
-    })
+      .done(function(result) {
+        $.each(result.version, function(i, item) {
+          $('.bookAndChapter--selectBook').append('<option>' + item.book_name + '</option>');
+        })
+      })
   }
 
   var retrieveChapters = function(book) {
-    $.ajax(url, {dataType:'jsonp',data: 'p=' + book,type:'GET'})
-    .done(function(result){
-      $.each(result.book, function(i,item) {
-        $('.bookAndChapter--selectChapter').append('<option>'+item.chapter_nr+'</option>');
+    $.ajax(url, {
+        dataType: 'jsonp',
+        data: 'p=' + book,
+        type: 'GET'
       })
-    });
+      .done(function(result) {
+        $.each(result.book, function(i, item) {
+          $('.bookAndChapter--selectChapter').append('<option>' + item.chapter_nr + '</option>');
+        })
+      });
   }
 
-  var retrieveVerse = function(version,book,chapter,verseNum) {
+  var retrieveVerse = function(version, book, chapter, verseNum) {
     var passage = book + chapter;
     if (!!verseNum) {
       passage += ':' + verseNum;
     }
-    $.ajax(url, {dataType:'jsonp',data:'p=' + passage + '&v=' + version,type:'GET'})
-    .done(function(result){
-      displayVerse(result);
-    })
-    .fail(function(jqXHR, message, error){ //this waits for the ajax to return with an error promise object
-      console.log(message + ' - ' + error);
-    });
+    $.ajax(url, {
+        dataType: 'jsonp',
+        data: 'p=' + passage + '&v=' + version,
+        type: 'GET'
+      })
+      .done(function(result) {
+        displayVerse(result);
+      })
+      .fail(function(jqXHR, message, error) { //this waits for the ajax to return with an error promise object
+        console.log(message + ' - ' + error);
+      });
   };
 
   var displayVerse = function(retrievedVerse) {
+    $('.item--firstLanguage').children('p').empty();
     if (retrievedVerse.type === 'chapter') {
       console.log(retrievedVerse.chapter);
-      $.each(retrievedVerse.chapter, function(i,item) {
+
+      $.each(retrievedVerse.chapter, function(i, item) {
         $('.item--firstLanguage').children('p').append('<strong>' + item.verse_nr + '</strong> ' + item.verse + '</br>');
       })
     } else {
-      $.each(retrievedVerse.book, function(i,item) {
-        $.each(item.chapter, function(i,item) {
+      $.each(retrievedVerse.book, function(i, item) {
+        $.each(item.chapter, function(i, item) {
           $('.item--firstLanguage').children('p').append('<strong>' + item.verse_nr + '</strong> ' + item.verse + '</br>');
         })
       })
@@ -66,7 +80,7 @@ $(function() {
   $('.getScripture').click(function() {
     verseNum = $('.verse').val();
     console.log(verseNum);
-    retrieveVerse(version,book,chapter,verseNum)
+    retrieveVerse(version, book, chapter, verseNum)
   });
 
   // retrieveVerse('kjv','John','3','16-18');
